1ee0c2b49694ac7b5ed25b7568586dae71dc29d2
Sridhar <09sridhar09@gmail.com>
2026-01-11 22:18:29 +0530

    feat: configure secure database connection using env vars

--- src/test/java/com/project/Microbussiness/MicrobussinessApplicationTests.java
+++ src/test/java/com/project/Microbussiness/MicrobussinessApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicrobussinessApplicationTests {
+class MicrobussinessApplication
+void contextLoads() {
+}
+
+}Tests {
 
 	@Test
-	void contextLoads() {
-	}
-
-}
